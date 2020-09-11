a4e1c95661f4abe400840aed68308c38054541a0
Alexey <lasrtg1@googlemail.com>
2020-09-11 14:41:02 +0300

    app

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import torch
+from inference import create_loader, predict
+from model import TweetModel
+
+PATH = "../input/qamodel/model.pth"
+
+@st.cache
+def main(path):
+    model = torch.load(path, map_location=torch.device('cpu'))
+    model.eval()
+    return model
 
 st.title('Tweet Sentiment Extraction')
 
@@ -7,5 +18,9 @@ sentiment = st.radio("Pick sentiment", ('Positive', 'Neutral', 'Negative'))
 tweet = st.text_area('Enter tweet')
 
 extract = st.button('Extract support phrases')
+if extract:
+    model = main(PATH)
+    loader = create_loader(sentiment, tweet)
+    output = predict(loader, model)
 
-st.text_area('Support phrases')
+phrases = st.text_area('Support phrases', value=output[0])
