b5cf463308ad4070b4ff0e57fc75aaf79981707d
Alexey <lasrtg1@googlemail.com>
2020-09-15 14:40:08 +0300

    form

--- src/inference.py
+++ src/inference.py
@@ -6,23 +6,22 @@ import torch
 from dataset import TweetDataset
 from train import calculate_jaccard_score
 
+
 def create_loader(tweet: str, sentiment: str):
-    
-    df = pd.DataFrame({'text': tweet, 'sentiment': sentiment}, index=[1])
-    
+
+    df = pd.DataFrame({"text": tweet, "sentiment": sentiment}, index=[1])
+
     test_dataset = TweetDataset(
         tweet=df.text.values,
         sentiment=df.sentiment.values,
-        selected_text=df.text.values
+        selected_text=df.text.values,
     )
-    
-    test_data_loader = DataLoader(
-        test_dataset,
-        batch_size=1
-    )
+
+    test_data_loader = DataLoader(test_dataset, batch_size=1)
     return test_data_loader
 
-def predict(data_loader, model, device=torch.device('cpu')):
+
+def predict(data_loader, model, device=torch.device("cpu")):
     final_output = []
     with torch.no_grad():
         for _, d in enumerate(data_loader):
@@ -43,11 +42,9 @@ def predict(data_loader, model, device=torch.device('cpu')):
             targets_end = targets_end.to(device, dtype=torch.long)
 
             outputs_start, outputs_end = model(
-                ids=ids,
-                mask=mask,
-                token_type_ids=token_type_ids
+                ids=ids, mask=mask, token_type_ids=token_type_ids
             )
-            
+
             for px, tweet in enumerate(orig_tweet):
                 selected_tweet = orig_selected[px]
                 tweet_sentiment = sentiment[px]
@@ -57,7 +54,7 @@ def predict(data_loader, model, device=torch.device('cpu')):
                     sentiment_val=tweet_sentiment,
                     idx_start=np.argmax(outputs_start[px, :]),
                     idx_end=np.argmax(outputs_end[px, :]),
-                    offsets=offsets[px]
+                    offsets=offsets[px],
                 )
                 final_output.append(output_sentence)
     return final_output
--- src/streamlit.py
+++ src/streamlit.py
@@ -5,22 +5,25 @@ from model import TweetModel
 
 PATH = "../input/qamodel/model.pth"
 
+
 @st.cache
 def main(path):
-    model = torch.load(path, map_location=torch.device('cpu'))
+    model = torch.load(path, map_location=torch.device("cpu"))
     model.eval()
     return model
 
-st.title('Tweet Sentiment Extraction')
 
-sentiment = st.radio("Pick sentiment", ('Positive', 'Neutral', 'Negative'))
+st.title("Tweet Sentiment Extraction")
 
-tweet = st.text_area('Enter tweet')
+sentiment = st.radio("Pick sentiment", ("Positive", "Neutral", "Negative"))
 
-extract = st.button('Extract support phrases')
+tweet = st.text_area("Enter tweet")
+
+extract = st.button("Extract support phrases")
 if extract:
     model = main(PATH)
     loader = create_loader(sentiment, tweet)
     output = predict(loader, model)
 
-phrases = st.text_area('Support phrases', value=output[0])
+phrases = st.text_area("Support phrases", value=output[0])
+
